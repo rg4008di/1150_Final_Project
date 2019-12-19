76fa7c58830e2a7464fdb7591d224fbd98ce1922
rg4008di <sreum0407@hotmail.com>
2019-12-19 16:22:17 -0600

    unsplash update

--- unsplash_api.py
+++ unsplash_api.py
@@ -1,13 +1,13 @@
 # access key (unsplash) e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa
 import requests
 #GET /photos/random
-url = 'https://api.unsplash.com/photos/random/?client_id'
+url = 'https://api.unsplash.com/photos/random?'
 
 query = 'tacos'
 
-client_id = 'e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa'
+client_id = e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa
 
-query_params = {'e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa': client_id, 'query': query}
+query_params = {'client_id': e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa, 'query': tacos}
 
 response = requests.get(url, params=query_params).json()
 
