47c93af02f593fa934d776ffe5e5996c7a5233ee
rg4008di <sreum0407@hotmail.com>
2019-12-19 15:07:34 -0600

    initial commit of the image

--- unsplash_api.py
+++ unsplash_api.py
@@ -1,7 +1,7 @@
 # access key (unsplash) e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa
 import requests
 #GET /photos/random
-url = 'https://api.unsplash.com/photos/random'
+url = 'https://api.unsplash.com/photos/random/?client_id'
 
 query = 'tacos'
 
@@ -10,3 +10,5 @@ client_id = 'e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa'
 query_params = {'e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa': client_id, 'query': query}
 
 response = requests.get(url, params=query_params).json()
+
+print(response)
