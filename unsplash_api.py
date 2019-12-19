39f603793e69c81678953755807c9092f44aebb5
rg4008di <sreum0407@hotmail.com>
2019-12-19 17:18:51 -0600

    unsplash update

--- unsplash_api.py
+++ unsplash_api.py
@@ -1,14 +1,24 @@
 # access key (unsplash) e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa
 import requests
-#GET /photos/random
-url = 'https://api.unsplash.com/photos/random?'
+from PIL import Image
+from io import BytesIO
+
+url = 'https://api.unsplash.com/photos/random/'
 
 query = 'tacos'
 
-client_id = e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa
+client_id = 'e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa'
 
-query_params = {'client_id': e73fdc2f4eff1bbeb0ec4505b7cc355b6572b434a09f410702383be7536129aa, 'query': tacos}
+query_params = {'client_id': client_id, 'query': query}
 
 response = requests.get(url, params=query_params).json()
 
-print(response)
+full_url = response['urls']['full']
+
+img_data = requests.get(full_url)
+
+img = Image.open(BytesIO(img_data.content))
+
+img.show()
+
+
